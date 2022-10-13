809913dea52cb068c8bd6c9e51e29bd05992af62
elnur <91825444+e1graf@users.noreply.github.com>
2022-10-13 13:46:50 +0300

    Add files via upload

---
+++ 1678A.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <array>
+
+
+int main()
+{
+	int t = 0;
+	int n = 0;
+	int k = 0;
+	int c = 0;
+	int j = 0;
+	int f = 0;
+	int const sarr = 100;
+	int i = 0;
+	std::cin >> t;
+	while (t--) {
+	    c = 0;
+		std::array<int, sarr> a{ 0 };
+		std::cin >> n;
+		for (i = 0; i < n; i++) {
+			std::cin >> a[i];
+			if (a[i] == 0) {
+				c++;
+			}
+		}
+		if (c == 0) {
+			for (i = 0; i < n - 1; i++) {
+				for (j = i + 1; j < n;j++) {
+					if (a[i] == a[j]) {
+						f = 1;
+						break;
+					}
+				}
+			}
+			if (f == 1) {
+				k = n;
+			}
+			else {
+				k = n + 1;
+			}
+		}
+		else {
+			k = n - c;
+		}
+		std::cout << k << std::endl;
+	}
+	return 0;
+}
